b2cf6f831ed309a94b7f47cd6bd228cc25065030
GAKU27 <gaku.tomiguchi@gmail.com>
2026-02-06 13:22:56 +0900

    変更

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'tempo-calculator-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './tempo-calculator.css',
+    './tempo-calculator.js',
+    './apple-touch-icon.png'
+];
+
+// Install Event: Cache default files
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event: Serve from cache, fall back to network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
